ec104986d65159943d4695449dd4468e41cb4d69
thiagolaurentino <thiago@t.com>
2024-05-28 20:46:14 -0300

    commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <bits/stdc++.h> 
 
 using namespace std;
 
@@ -10,12 +11,21 @@ int main()
     fstream arquivo;
     string nomeArquivo = "arquivo.txt";
     string texto;
+    string token;
+   // stringstream ss;
 
     arquivo.open(nomeArquivo, ios::out|ios::app|ios::in);
 
+    arquivo << "Prof Natan" << endl;
+    arquivo.seekg(0);
     while(getline(arquivo, texto))
     {
-        cout << texto << endl;
+        stringstream ss(texto);
+        while(getline(ss,token,' ')){
+            cout << "Nome: " << token;
+
+        }
+       
     }
 
     arquivo.close();
